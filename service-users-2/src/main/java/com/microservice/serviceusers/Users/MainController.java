636eaab3f109f2e9ad65773855655c4ea4bf8121
norullah <norullah266.nk86@gmail.com>
2023-07-30 00:03:16 +0430

    [adding] dockerization is done, zipkin added, and some other stuff

--- service-users-2/src/main/java/com/microservice/serviceusers/Users/MainController.java
+++ service-users-2/src/main/java/com/microservice/serviceusers/Users/MainController.java
@@ -3,6 +3,7 @@ package com.microservice.serviceusers.Users;
 
 import com.microservice.serviceusers.Posts.Posts;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,14 +18,14 @@ public class MainController {
     }
 
     @GetMapping("/getUsersPosts")
-    @CircuitBreaker(name = "mycircuit", fallbackMethod = "connectionFail")
+    @Retry(name = "mycircuit", fallbackMethod = "connectionFail")
     public Posts usersPosts(){
         ResponseEntity<Posts> getAllPosts = new RestTemplate().getForEntity(
                         "http://localhost:8082/posts", Posts.class);
 //        "http://localhost:8765/SERVICE-POSTS/posts", Post.class);
         return getAllPosts.getBody();
     }
-    public String connectionFail(Exception exception){
-        return "Oops, the service posts are have the problem ";
+    public Posts connectionFail(Throwable throwable){
+        return new Posts(); // or any other fallback implementation that returns a valid Posts object
     }
 }
